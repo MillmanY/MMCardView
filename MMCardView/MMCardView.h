e96ffcb7355bff32dd6d1fd9a54620458299bb30
David Collado <bitomule@gmail.com>
2017-02-23 12:51:42 +0100

    carthage support

---
+++ MMCardView/MMCardView.h
@@ -0,0 +1,19 @@
+//
+//  MMCardView.h
+//  MMCardView
+//
+//  Created by David Collado on 23/2/17.
+//  Copyright © 2017 MillmanY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMCardView.
+FOUNDATION_EXPORT double MMCardViewVersionNumber;
+
+//! Project version string for MMCardView.
+FOUNDATION_EXPORT const unsigned char MMCardViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMCardView/PublicHeader.h>
+
+
